a25701cf8022bc8f76aeda17ab8da2593ced6b16
jyoshnavi <abc@gmail.com>
2025-11-13 09:29:14 +0530

    fixed input function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-num = input("enter the number")
+num =int( input("enter the number"))
 if a % 2 ==0:
-	print("it is an even number")
-else:
-	print("it is an odd number")
+    print("it is an even number")
+else:	
+    print("it is an odd number")
